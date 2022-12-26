7c49893be566a695829718b36ec90761b7999629
Ol-k121967 <oleg.koriakoff@yandex.ru>
2022-12-26 18:43:59 +0300

    add btn_generace btn_reset

--- JS/init.js
+++ JS/init.js
@@ -1,7 +1,16 @@
-
-window.onload = function()
-{
+document.getElementById('btn_generace').addEventListener('click', function () {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-};
+})
+
+
+document.getElementById('btn_reset').addEventListener('click', function () {
+    document.getElementById('firstNameOutput').innerText = 'firstName';
+    document.getElementById('secondNameOutput').innerText = 'secondNameOutput';
+    document.getElementById('genderOutput').innerText = 'genderOutput';
+    document.getElementById('birthYearOutput').innerText = 'birthYearOutput';
+    document.getElementById('surnameOutput').innerText = 'surnameOutput';
+})
+
+
 
